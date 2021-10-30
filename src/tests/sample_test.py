be026422d389381fa157f3322a83743d98fb4e71
esraa-saleh <esraasaleh@cmail.carleton.ca>
2021-10-30 12:09:27 -0400

    Add modified slurm.py and tests folder

---
+++ src/tests/sample_test.py
@@ -0,0 +1,7 @@
+# Testing files should be in files like this.
+# TODO: show sample tests with pytest
+# But it doesn't need to be with pytest. Do whatever you're comfortable with because this is about
+# rapid prototyping.
+
+
+
