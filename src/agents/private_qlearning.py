5ea1db07607ff976fb7a77effd5c0e5f97c99652
eoz-imaginary <zoe.riell@gmail.com>
2021-10-31 18:58:56 -0600

    Finished formatting & detangling DQN from DQNAgent (no tests yet)

--- src/agents/private_qlearning.py
+++ src/agents/private_qlearning.py
@@ -2,16 +2,13 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import optim
 import bisect
+import random
+import math
 from src.environments.hitorstandcontinuous import hitorstandcontinuous
-from src.utils.optimize_model import optimize_model
 from src.utils.ReplayMemory import ReplayMemory
 from src.utils.ReplayMemory import Transition
-from src.utils.select_action import select_action
-
-#PLACEHOLDER ENV (NIDHI'S)
-env = hitorstandcontinuous()
-m = env.action_space.n
 
 
 #NOISEBUFFER && NOISEBUFFER HELPER METHODS
@@ -82,20 +79,18 @@ class noisebuffer:
     def reset(self):
         self.buffer = []
 
-#AGENT
+#NETWORK
 class DQN(nn.Module):
-    def __init__(self, sigma=0.4, hidden=16):
+    def __init__(self, m, hidden=16, sigma=0.4):
         super(DQN, self).__init__()
         self.linear1 = nn.Linear(1, hidden)
         self.linear2 = nn.Linear(hidden, hidden)
         self.head = nn.Linear(hidden, m)
         self.sigma = sigma
-        self.nb = noisebuffer(2, sigma)
         self.memory = None
         self.total_reward = 0
+        self.nb = noisebuffer(m, sigma)
 
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
     def forward(self, s):
         x = F.relu(self.linear1(s))
         x = F.relu(self.linear2(x))
@@ -107,9 +102,24 @@ class DQN(nn.Module):
         else:
             return x
 
-    #init ReplayMemory with specified capacity
-    def agent_set_memory(self, capacity):
-        self.memory = ReplayMemory(capacity)
+#AGENT
+class DQNAgent():
+    def __init__(self, episode, m, EPS_START=0.9, EPS_END=0.05, EPS_DECAY=200, TARGET_UPDATE=10, BATCH_SIZE=128, GAMMA=0.99):
+        self.memory = ReplayMemory(10000)
+        self.total_reward = None
+        self.state = None
+        self.action = None
+        self.episode = episode
+        self.BATCH_SIZE = BATCH_SIZE
+        self.GAMMA = GAMMA
+        self.EPS_START = EPS_START
+        self.EPS_END = EPS_END
+        self.EPS_DECAY = EPS_DECAY
+        self.TARGET_UPDATE = TARGET_UPDATE
+        self.STEPS_DONE = 0
+        self.m = m
+        self.policy_net = DQN(self.m)
+        self.target_net = DQN(self.m)
 
     #getters for ReplayMemory and total reward
     def agent_get_memory(self):
@@ -118,38 +128,104 @@ class DQN(nn.Module):
     def agent_get_total_reward(self):
         return self.total_reward
 
+    def select_action(self, state):
+        steps_done = 0
+        eps_start = 0.9
+        eps_end = 0.05
+        eps_decay = 200
+        sample = random.random()
+        eps_threshold = self.EPS_END + (self.EPS_START - self.EPS_END) * \
+                        math.exp(-1. * self.STEPS_DONE / self.EPS_DECAY)
+        steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # t.max(1) will return largest value for column of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(state).max(1)[1].view(1, 1)
+        else:
+            return torch.tensor([[random.randrange(self.m)]], dtype=torch.long)
+
+    def optimize_model(self, device="cpu"):
+
+        optimizer = optim.RMSprop(self.policy_net.parameter())
+        if len(self.memory) < self.BATCH_SIZE:
+            return
+        transitions = self.memory.sample(self.BATCH_SIZE)
+
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                                batch.next_state)), device=device, dtype=torch.uint8)
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                           if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        next_state_values = torch.zeros(self.BATCH_SIZE, device=device)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
+
+        expected_state_action_values = (next_state_values * self.GAMMA) + reward_batch
+
+        # Compute Huber loss
+        loss = F.smooth_l1_loss(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        optimizer.zero_grad()
+        loss.backward()
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        optimizer.step()
+
     #RL Glue methods:
-    def agent_start(self, state, net):
-        action = select_action(state, net, m)
+    def agent_start(self, state):
+        self.total_reward = 0
+        self.state = torch.Tensor(state).unsqueeze(0)
+        action = self.select_action(state)
+        self.action = action
         return action
 
     #added replaymemory as a parameter here since Nidhi's update loop expects the memory as a global variable
-    def agent_step(self, state, reward, net, target_net):
-
-        #choose action from current state
-        action = select_action(state, net, m)
-        #extract next state info
-        next_state, next_reward, done, info = env.step(action.item())
-        next_state = torch.Tensor(next_state).unsqueeze(0)
+    def agent_step(self, next_state, reward):
 
         #manually putting in device='cpu' here to avoid having to pass it in
-        next_reward = torch.tensor([reward], device='cpu')
-
+        reward = torch.tensor([reward], device='cpu')
+        reform_next_state = torch.Tensor(next_state).unsqueeze(0)
         #store transition in memory
         memory = self.agent_get_memory()
-        memory.push(state, action, next_state, next_reward)
+        memory.push(self.state, self.action, reform_next_state, reward)
 
         # recieve reward
         self.total_reward += float(reward.squeeze(0).data)
 
         #move to next state
-        state = next_state
+        self.state = reform_next_state
 
         #optimize model
-        optimize_model(memory, net, target_net, Transition, BATCH_SIZE=128, GAMMA=0.99, device='cpu')
+        self.optimize_model()
 
-        return action
+        #select next action here
+        next_action = self.select_action(reform_next_state)
+        self.action = next_action
+
+        return next_action
 
     def agent_end(self, reward):
-        pass
+        if self.episode % self.TARGET_UPDATE == 0:
+            self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.memory.push(self.state, self.action, None, reward)
+        reward = torch.tensor([reward], device='cpu')
+        self.total_reward += float(reward.squeeze(0).data)
+        self.state = None
+
+        self.optimize_model()
+
+
+
 
