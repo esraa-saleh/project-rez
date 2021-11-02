8e959aa02c934f78507e07eaee6b9fffb3652536
Rohini Das <rohinidas.911@gmail.com>
2021-11-02 17:15:25 -0600

    minor changes

--- src/environments/ContinuousChain.py
+++ src/environments/ContinuousChain.py
@@ -1,22 +1,30 @@
 #from src.environments.env import BaseEnvironment
 from copy import deepcopy
+from gym.spaces.box import Box
+from gym.spaces.discrete import Discrete
+import numpy as np
 # from mypy_extensions import TypedDict
 
 
 
-class ContinuousChain():
+class ContinuousChain:
 
 
-    def __init__(self, env_info={}):
+    def __init__(self, sparsity, seed, max_episode_len):
         """Setup for the environment called when the experiment first starts.
         Note:
             Initialize a tuple with the reward, first state, boolean
             indicating if it's terminal.
         """
-        # a term idx is a 1d indexing of the grid that includes terminal states
-        # a nonterm idx is the same but it skips terminal states in its counting
+        self.observation_space = Box(0*np.ones(1), 1.0*np.ones(1), dtype=np.float64)
+        self.action_space = Discrete(2)
+        self.cnt = 0
+        self.length = max_episode_len
+        self.sparsity = sparsity
+        self.rng = np.random.RandomState(seed)
+        
 
-        super().__init__()
+        #super().__init__()
         pass
 
     def env_start(self):
